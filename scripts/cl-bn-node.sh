5158c7c56bef8b83ce4144369cb2af45fe722ccb
Akash <mailmeakashhalder@gmail.com>
2024-03-05 17:42:49 +0530

    test

--- scripts/cl-bn-node.sh
+++ scripts/cl-bn-node.sh
@@ -9,7 +9,8 @@ cleanup() {
 
 trap cleanup EXIT
 
-BASE_CL_PORT=$31000
+BASE_CL_PORT=31000
+
 index=$1
 
 # Function to find an available port
@@ -23,9 +24,9 @@ find_available_port() {
 
 cl_data_dir $index
 datadir=$cl_data_dir
-port=$($BASE_CL_PORT + $index)
-echo $index
-echo $port
+port=$(find_available_port $BASE_CL_PORT)
+echo "Selected port for libp2p communication: $port"
+
 http_port=$(expr $BASE_CL_HTTP_PORT + $index)
 log_file=$datadir/beacon_node.log
 
