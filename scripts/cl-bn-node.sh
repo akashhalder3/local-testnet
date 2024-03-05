d90212ac732e4fe504bab8a54d08a8bf1dad3596
Akash <mailmeakashhalder@gmail.com>
2024-03-05 14:37:35 +0530

    test

--- scripts/cl-bn-node.sh
+++ scripts/cl-bn-node.sh
@@ -14,7 +14,7 @@ index=$1
 cl_data_dir $index
 datadir=$cl_data_dir
 port=$(expr $BASE_CL_PORT + $index)
-http_port=$(expr $BASE_CL_HTTP_PORT + $index)
+http_port=$(expr $BASE_CL_HTTP_PORT + $index + 1)
 echo $http_port
 log_file=$datadir/beacon_node.log
 
