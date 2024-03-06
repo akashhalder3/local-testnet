95670614cd242793547c865e3f441614c59c2cbc
Akash <mailmeakashhalder@gmail.com>
2024-03-06 16:06:13 +0530

    test

--- scripts/cl-bn-node.sh
+++ scripts/cl-bn-node.sh
@@ -19,7 +19,7 @@ log_file=$datadir/beacon_node.log
 
 # If index is 2, add 5 to the port
 if [[ $index -eq 2 ]]; then
-    port=$((BASE_CL_PORT + 5))
+    port=$((BASE_CL_PORT + 1))
 else
     port=$((BASE_CL_PORT + index))
 fi
