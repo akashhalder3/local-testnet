e6cf5dbb19bc44792da2d4c3a4c81bbd641c1091
Akash <mailmeakashhalder@gmail.com>
2024-03-06 13:13:04 +0530

    global

--- scripts/signer-node.sh
+++ scripts/signer-node.sh
@@ -18,19 +18,18 @@ log_file=$datadir/geth.log
 echo "Started the geth node 'signer' which is now listening at port $SIGNER_PORT. You can see the log at $log_file"
 $GETH_CMD \
     --datadir $datadir \
-    --authrpc.addr=0.0.0.0 \
     --authrpc.port $SIGNER_RPC_PORT \
     --port $SIGNER_PORT \
     --http \
     --http.api=eth,net,web3,debug,txpool \
     --http.addr=0.0.0.0 \
     --http.corsdomain="*" \
-    --http.port $SIGNER_HTTP_PORT \
     --ws \
     --ws.api=eth,net,web3,debug,txpool \
     --ws.addr=0.0.0.0 \
-    --ws.port=$SIGNER_WS_PORT
     --ws.origins="*" \
+    --ws.port=$SIGNER_WS_PORT \
+    --http.port $SIGNER_HTTP_PORT \
     --allow-insecure-unlock \
     --bootnodes $boot_enode \
     --networkid $NETWORK_ID \
@@ -38,7 +37,6 @@ $GETH_CMD \
     --password $ROOT/password \
     --mine \
     --miner.etherbase $address \
-    --nat extip:20.244.97.158 \
     < /dev/null > $log_file 2>&1
 
 if test $? -ne 0; then
