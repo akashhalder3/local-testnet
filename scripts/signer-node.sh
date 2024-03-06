89f05355053c15dd57c4f57b4c60acbe1abc2370
Akash <mailmeakashhalder@gmail.com>
2024-03-06 13:03:47 +0530

    global

--- scripts/signer-node.sh
+++ scripts/signer-node.sh
@@ -18,6 +18,7 @@ log_file=$datadir/geth.log
 echo "Started the geth node 'signer' which is now listening at port $SIGNER_PORT. You can see the log at $log_file"
 $GETH_CMD \
     --datadir $datadir \
+    --authrpc.addr=0.0.0.0 \
     --authrpc.port $SIGNER_RPC_PORT \
     --port $SIGNER_PORT \
     --http \
@@ -28,6 +29,7 @@ $GETH_CMD \
     --ws \
     --ws.api=eth,net,web3,debug,txpool \
     --ws.addr=0.0.0.0 \
+    --ws.port=$SIGNER_WS_PORT
     --ws.origins="*" \
     --allow-insecure-unlock \
     --bootnodes $boot_enode \
